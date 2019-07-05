7b5547c76cf8920073b68debb40087f03e747ab0
renato-sugiyama <qx24.rs2357@gmail.com>
2019-07-05 15:45:08 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,19 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/current"
+working_directory app_path
 
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 timeout 60
+
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
@@ -23,7 +27,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 
   if run_once
-    run_once = false # prevent from firing again
+    run_once = false
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
